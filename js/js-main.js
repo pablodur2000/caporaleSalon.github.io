604c954b15cd71aab579dd13f8822c2f509d4c6f
pablodur2000 <pablod082000@gmail.com>
2024-02-20 17:52:14 -0300

    Alfa 20/02/24

--- js/js-main.js
+++ js/js-main.js
@@ -157,6 +157,68 @@ function slides(){
 
 }
 
+const elements = document.getElementsByClassName('bar');
+
+for (let i = 0; i < elements.length; i++) {
+    elements[i].addEventListener('click', () => {
+        const containerBar = elements[i].querySelector('.container-bar');
+        const isActive = containerBar.classList.contains('active-bar');
+
+        // Oculta todos los contenedores de barra antes de mostrar el actual
+        const allContainerBars = document.querySelectorAll('.container-bar');
+        allContainerBars.forEach(container => {
+           
+            container.classList.remove('active-bar');
+            
+        });
+
+        // Si el contenedor actual no está activo, muéstralo; de lo contrario, ocúltalo
+        
+        if (!isActive) {
+            containerBar.classList.toggle('active-bar');
+            
+        }
+    });
+}
+
+
+
+
+/*
+//---------------------------frequent questions-----------------------
+document.querySelectorAll('.bar').forEach(container_visible => {
+    container_visible.addEventListener('click', () =>{
+        const content = container_visible.querySelector('.container-bar');
+        
+        const arrowInactive = container_visible.querySelector('.inactive');
+        const arrowActive = container_visible.querySelector('.active');
+
+        // Verificar si otro contenido está desplegado
+        const otherContents = document.querySelectorAll('.container-visible');
+        otherContents.forEach(otherContent => {
+            if (otherContent !== content) {
+                otherContent.classList.remove('container-visible');
+                otherContent.style.maxHeight = 0;
+            }
+        });
+
+        // Alternar la visibilidad del contenido actual
+        content.classList.toggle('container-visible');
+        if (content.classList.contains('container-visible')) {
+            content.style.maxHeight = content.scrollHeight + "px";
+        } else {
+            content.style.maxHeight = 0;
+        }
+
+        // Alternar las clases de las flechas
+        arrowInactive.classList.toggle('inactive-click');
+        arrowActive.classList.toggle('active-click');
+    });
+});
+
+*/
+
+
 /*
 
 for( let i = 1; i <= 7; i++){
