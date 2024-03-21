723d482327aa3cd36437defbaa89d529730c8d20
pablodur2000 <pablod082000@gmail.com>
2024-03-21 18:49:34 -0300

    Alfa 21.3.2024

--- js/js-curse.js
+++ js/js-curse.js
@@ -41,29 +41,99 @@ for (let i = 0; i < h2Prices.length; i++){
 
 //-----------End Prices------------
 
+//---------------------volume on---------------------
+
+document.addEventListener("DOMContentLoaded", function() {
+    var botton = document.querySelectorAll('.curse-content-info-top-testimony-volumeOn');
+    var video = document.getElementById("curse-content-info-top-testimony-video");
+    var buttonMute = document.querySelectorAll('.fa-volume-xmark');
+    var mute = true;
+
+    botton[0].addEventListener('click', function() {
+        if (mute == true){
+            video.muted = false;
+            botton[0].style.top = '10%';
+            botton[0].style.backgroundColor = 'black';
+            buttonMute[0].style.color = 'white';
+            buttonMute[0].style.opacity = '1';
+            video.style.filter = 'brightness(1) blur(0px)';
+            mute = false;
+        }else{
+            video.muted = true;
+            botton[0].style.top = '50%';
+            botton[0].style.backgroundColor = 'white';
+            buttonMute[0].style.color = 'white';
+            buttonMute[0].style.opacity = '0';
+            video.style.filter = 'brightness(0.3) blur(1px)';
+            mute = true;
+        }
+        
+
+    });
+
+});
+
+
 
 //-------------------Movimientos página-----------------
 
 document.addEventListener("DOMContentLoaded", function() {
     var titleCurse = document.querySelectorAll('.title-curse');
+    var imgCover = document.getElementById('img-cover');
     var containersTopIndividual = document.querySelectorAll('.curse-container-top-info-individual');
     var containerTopInfo = document.querySelectorAll('.curse-container-top-info');
+    var contentIndividual = document.querySelectorAll('.curse-container-medium-inter-individual');
+    var contentIndividualP = document.querySelectorAll('.curse-container-medium-inter-individual-p');
+    var imgPrices = document.querySelectorAll('.curse-container-bottom-prices-img');
+    var wppPrices = document.querySelectorAll('.curse-container-bottom-prices-bottom-wpp-container');
+
 
     var windowHeight = window.innerHeight;
     var windowWidth = window.innerWidth;
 
     function checkVisibility(){
         titleCurse[0].style.opacity = '1';
+        imgCover.style.filter = 'none'
 
         for (let i = 0; i < containersTopIndividual.length; i++){
 
             var boundingcontainersTopIndividual = containersTopIndividual[i].getBoundingClientRect();
-            
-            if (boundingcontainersTopIndividual.top >= 0 && boundingcontainersTopIndividual.top <= windowHeight - 200){
+            var medida;
+            if (windowWidth < 1200){
+                medida = 0;
+            }else{
+                medida = 160;
+            }
+
+            if (boundingcontainersTopIndividual.top >= 0 && boundingcontainersTopIndividual.top <= windowHeight - medida){
                 containerTopInfo[0].style.backdropFilter = 'blur(4px)';
                 containersTopIndividual[i].style.opacity = '1';
                 containersTopIndividual[i].style.transform = 'translateY(0)';
             }
+        }
+
+        for (let i = 0; i < contentIndividual.length; i++){
+
+            var boundingcontentIndividual = contentIndividual[i].getBoundingClientRect();
+            
+
+            if (boundingcontentIndividual.top >= 0 && boundingcontentIndividual.top <= windowHeight - 200){
+                contentIndividualP[i].style.opacity = '1';
+
+                setTimeout(function (){  
+                    contentIndividual[i].style.backgroundColor = 'rgba(142, 142, 142)';
+                }, 300);
+            }
+        }
+
+        var boundingImgPrices = imgPrices[0].getBoundingClientRect();
+            
+        if (boundingImgPrices.top >= 0 && boundingImgPrices.top <= windowHeight - 250){
+            imgPrices[0].style.filter = 'contrast(1.2)';
+
+            setTimeout(function (){  
+                wppPrices[0].style.opacity = '1';
+            }, 1200);
         }
 
     }
